56c62a361b0122e9c7603b8d5a9ba8ffab89a46b
Abdifatah Hassan <abdifatah.hassan@bwscloud.tech>
2025-12-19 22:54:15 +0200

    Add second route and JSON middleware

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
+  console.log('res', res)
   res.json({ status: "ok" });
+});
+
+app.get("/hello", (req, res) => {
+    console.log('res', res)
+  res.json({ message: "Hello from backend" });
 });
 
 app.listen(3000, () => {
